433dae23f5936a761a9c2b0429e722e97cc3aeb4
Tyler Langlois <tjl@byu.net>
2016-11-01 13:32:21 -0400

    Add helper puppet function to derive elasticsearch version

---
+++ lib/puppet/parser/functions/guess_es_version.rb
@@ -0,0 +1,30 @@
+module Puppet::Parser::Functions
+  newfunction(
+    :guess_es_version,
+    :type => :rvalue,
+    :doc => <<-'ENDHEREDOC') do |args|
+      Given a number of arguments, attempt to determine which version of
+      Elasticsearch is defined in the first string that can be parsed.
+
+      Example:
+
+        guess_es_version('unused', 'version/string/2.4.1.ext')
+        # Would return: "2.4.1"
+    ENDHEREDOC
+
+    if args.length < 1
+      raise Puppet::ParseError, ("guess_es_version(): wrong number of arguments (#{args.length}; must be at least 1)")
+    end
+
+    args.each do |str|
+      next unless str.is_a? String
+
+      # Father, forgive me
+      if /^(?<version>[0-9]+(?:\.[0-9a-z]+)*)$|-(?<version>[0-9.]+)\.[0-9a-z]+$/ =~ str
+        return version
+      end
+    end
+
+    raise Puppet::ParseError, 'could not determine Elasticsearch version'
+  end
+end
---
+++ spec/functions/guess_es_version_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe 'guess_es_version' do
+
+  describe 'exception handling' do
+    it { is_expected.to run.with_params().and_raise_error(
+      Puppet::ParseError, /wrong number of arguments/i
+    ) }
+  end
+
+  describe 'unguessable arguments' do
+    it { is_expected.to run.with_params(nil)
+      .and_raise_error Puppet::ParseError }
+
+    it { is_expected.to run.with_params('foobar', 'http://artifacts.elastic.co')
+      .and_raise_error Puppet::ParseError }
+
+    it { is_expected.to run.with_params('')
+      .and_raise_error Puppet::ParseError }
+  end
+
+  {
+    'typical versions' => Hash[*(
+      ['0.90.1', '1.7', '2.4.1', '5.x'].map{|x|[x,x]}.flatten
+    )],
+    'package urls' => {
+      'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.4.2.deb' => '1.4.2',
+      'puppet:///path/to/elasticsearch-2.4.1.deb' => '2.4.1',
+      'file:/path/to/elasticsearch-5.0.0.deb' => '5.0.0',
+      '/path/0.0/to/elasticsearch-2.4.1.deb' => '2.4.1',
+      '/path/to/elasticsearch-1-2.4.1.deb' => '2.4.1',
+      '/2.x/to/elasticsearch-5.0.0.deb' => '5.0.0'
+    }
+  }.each do |test_type, versions|
+    describe test_type do
+      versions.each do |pre_parsed, version|
+        it { is_expected.to run.with_params(pre_parsed)
+          .and_return(version) }
+
+        it { is_expected.to run.with_params(nil, pre_parsed)
+          .and_return(version) }
+
+        it { is_expected.to run.with_params(pre_parsed, nil)
+          .and_return(version) }
+
+        it { is_expected.to run.with_params(pre_parsed, '1.0.0')
+          .and_return(version) }
+
+        it { is_expected.to run.with_params('1.0.0', pre_parsed)
+          .and_return('1.0.0') }
+      end
+    end
+  end
+end
