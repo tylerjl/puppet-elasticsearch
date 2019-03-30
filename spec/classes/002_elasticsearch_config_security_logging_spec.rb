5acc8ed22d765d053877600f66fd5202372691b2
Tyler Langlois <tjl@byu.net>
2019-03-29 23:08:50 -0600

    Fix log parameter tests without shield settings

--- spec/classes/002_elasticsearch_config_security_logging_spec.rb
+++ spec/classes/002_elasticsearch_config_security_logging_spec.rb
@@ -1,44 +1,5 @@
 require 'spec_helper'
 
-shared_examples 'security plugin logging' do |plugin, logfile, tests|
-  describe "security logging configuration file for #{plugin}" do
-    tests.each_pair do |param_type, params|
-      context "with no security plugin defined for #{param_type}" do
-        let(:params) do
-          { "security_logging_#{param_type}" => params[:manifest] }
-        end
-
-        it { should_not compile.with_all_deps }
-      end
-
-      context "parameter #{param_type}" do
-        let(:params) do
-          {
-            :security_plugin => plugin,
-            "security_logging_#{param_type}" => params[:manifest]
-          }
-        end
-
-        it { should contain_file("/etc/elasticsearch/#{plugin}")
-          .with_ensure('directory')}
-
-        case param_type
-        when 'source'
-          it 'sets the source for the file resource' do
-            should contain_file("/etc/elasticsearch/#{plugin}/#{logfile}")
-              .with_source(params[:value])
-          end
-        when 'content'
-          it 'sets logging file yaml content' do
-            should contain_file("/etc/elasticsearch/#{plugin}/#{logfile}")
-              .with_content(params[:value])
-          end
-        end
-      end
-    end
-  end
-end
-
 describe 'elasticsearch', :type => 'class' do
   on_supported_os(
     :hardwaremodels => ['x86_64'],
@@ -55,17 +16,43 @@ describe 'elasticsearch', :type => 'class' do
         :common => ''
       ) }
 
-      include_examples 'security plugin logging',
-                       'x-pack',
-                       'log4j2.properties',
-                       'content' => {
-                         :manifest => "one = two\nfoo = bar\n",
-                         :value => "one = two\nfoo = bar\n"
-                       },
-                       'source' => {
-                         :manifest => '/foo/bar.properties',
-                         :value => '/foo/bar.properties'
-                       }
+      describe "security logging configuration file for x-pack" do
+        {
+          'content' => {
+            :manifest => "one = two\nfoo = bar\n",
+            :value => "one = two\nfoo = bar\n"
+          },
+          'source' => {
+            :manifest => '/foo/bar.properties',
+            :value => '/foo/bar.properties'
+          }
+        }.each_pair do |param_type, params|
+          context "parameter #{param_type}" do
+            let(:params) do
+              {
+                :security_plugin => 'x-pack',
+                "security_logging_#{param_type}" => params[:manifest]
+              }
+            end
+
+            it { should contain_file("/etc/elasticsearch/x-pack")
+                .with_ensure('directory')}
+
+            case param_type
+            when 'source'
+              it 'sets the source for the file resource' do
+                should contain_file("/etc/elasticsearch/x-pack/log4j2.properties")
+                  .with_source(params[:value])
+              end
+            when 'content'
+              it 'sets logging file yaml content' do
+                should contain_file("/etc/elasticsearch/x-pack/log4j2.properties")
+                  .with_content(params[:value])
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
